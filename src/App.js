bf397bc3673920075e5e100a97df52382e76e50c
pengyw <290295349@qq.com>
2019-06-22 01:20:42 +0800

    引入material-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -8,9 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Button variant="contained" color="primary">
+            按钮
+          </Button>
           <a
             className="App-link"
             href="https://reactjs.org"
