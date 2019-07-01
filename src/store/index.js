d490ffe13bd285c0886ee5dd21340f94ad5d3565
pengy <pengy@missfresh.cn>
2019-07-01 21:19:57 +0800

    回家

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { routerReducer } from 'react-router-redux';
+import reducers from './reducer';
+import logger from 'redux-logger';
+
+const originalReducers = {
+  ...reducers,
+  routing: routerReducer
+};
+
+const reducer = combineReducers(originalReducers);
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'production') {
+  console.log("生产环境");
+  middlewares.push(logger);
+}
+
+const initialState = {};
+const store = createStore(reducer, initialState, applyMiddleware(...middlewares));
+export default store;
