975e96574fca7a40cc5cbcec72118e469b806cf7
wangjunfa <466564750@qq.com>
2019-07-01 14:15:37 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import { Router, Route, Link } from 'react-router'
-import Home from './pages/home';
+import Home from './pages/home'
 import ErrorPage from './pages/error-page'
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import './App.css'
+import { Router, Route } from 'react-router-dom'
+const history = require('history').createBrowserHistory()
 class App extends Component {
-  render() {
+  render () {
     return (
-     <Router history="history">
-      <Route path="/home" component={Home}/>
-      <Route path="/error-page" component={ErrorPage}/>
-    </Router>
-    );
+      <Router history={history}>
+        <Route path='/' component={Home} />
+        <Route path='/home' component={Home} />
+        <Route path='/error-page' component={ErrorPage} />
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
